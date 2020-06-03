fa10380d4d5e679bec8e1bdd646b424d5980eed1
kuzentio <0585ec@gmail.com>
2020-06-03 22:22:52 +0300

    Remove recognition application

--- recognition/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from recognition.models import RecogniseRequest
-
-
-class RecogniseRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RecogniseRequest
-        fields = ('image', )

21a0946a6148f58337aba1e20fe117f7845b6dc3
kuzentio <0585ec@gmail.com>
2020-06-04 07:57:38 +0300

    Add validation for vegetable

--- vegetable/serializers.py
+++ vegetable/serializers.py
@@ -1,4 +1,6 @@
+from django.utils.text import slugify
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from vegetable.models import Vegetable
 
@@ -10,3 +12,9 @@ class VegetableSerializer(serializers.ModelSerializer):
         fields = ('pk', 'name', 'slug')
         read_only_fields = ('slug', )
 
+    def validate_name(self, attrs):
+        if len(attrs) <= 0:
+            raise ValidationError("Name can not be empty.")
+        if Vegetable.objects.filter(slug=slugify(attrs)).exists():
+            raise ValidationError("This product already exists.")
+        return attrs
--- vegetable/views.py
+++ vegetable/views.py
@@ -7,3 +7,8 @@ from vegetable.serializers import VegetableSerializer
 class VegetableListViewSet(viewsets.ModelViewSet):
     queryset = Vegetable.objects.all()
     serializer_class = VegetableSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super(VegetableListViewSet, self).create(request, *args, **kwargs)
+        print(response)
+        return response

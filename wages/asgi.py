c40f364ea4a86a21642678c1a5b0dafd7c86542b
kuzentio <0585ec@gmail.com>
2020-06-27 23:22:29 +0300

    Checkout settings

--- wages/asgi.py
+++ wages/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wages.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wages.settings.local')
 
 application = get_asgi_application()

9dcc82c1a87504d5b3d64455647c7bfa58f2aefc
kuzentio <0585ec@gmail.com>
2020-10-25 01:21:52 +0300

    Fix csrf exception

--- recognition/views.py
+++ recognition/views.py
@@ -2,7 +2,8 @@ import json
 import os
 from PIL import Image
 from detecto import core, utils
-from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 from vegetable import utils
 from vegetable.models import PYTORCH_BASE_DIR, Vegetable
@@ -10,17 +11,34 @@ from vegetable.models import PYTORCH_BASE_DIR, Vegetable
 PATH = os.path.join(PYTORCH_BASE_DIR, 'models', 'test.pth')
 
 
-def recognize(request):
-    model = core.Model.load(
-        PATH,
-        list(Vegetable.objects.all().order_by('id').values_list('slug', flat=True))
-    )
-    image_data = json.loads(request.body).get('image_data')
-    img_file = utils.decode_base64_file(image_data)
-    img = Image.open(img_file)
-    labels, boxes, scores = model.predict_top(img)
-    predictions = zip(labels, [s.item() for s in scores])
-    return JsonResponse({
-        'success': True,
-        'predictions': list(predictions),
-    })
+# def recognize(request):
+#     model = core.Model.load(
+#         PATH,
+#         list(Vegetable.objects.all().order_by('id').values_list('slug', flat=True))
+#     )
+#     image_data = json.loads(request.body).get('image_data')
+#     img_file = utils.decode_base64_file(image_data)
+#     img = Image.open(img_file)
+#     labels, boxes, scores = model.predict_top(img)
+#     predictions = zip(labels, [s.item() for s in scores])
+#     return JsonResponse({
+#         'success': True,
+#         'predictions': list(predictions),
+#     })
+
+
+class RecogniseViewSet(viewsets.ModelViewSet):
+    def retrieve(self, request, *args, **kwargs):
+        model = core.Model.load(
+            PATH,
+            list(Vegetable.objects.all().order_by('id').values_list('slug', flat=True))
+        )
+        image_data = json.loads(request.body).get('image_data')
+        img_file = utils.decode_base64_file(image_data)
+        img = Image.open(img_file)
+        labels, boxes, scores = model.predict_top(img)
+        predictions = zip(labels, [s.item() for s in scores])
+        return Response({
+            'success': True,
+            'predictions': list(predictions),
+        })

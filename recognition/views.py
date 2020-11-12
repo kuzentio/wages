e367ba237c937b1c834f15ced7216be37038804b
kuzentio <0585ec@gmail.com>
2020-11-12 20:40:49 +0200

    checkout recognition to CBV

--- recognition/views.py
+++ recognition/views.py
@@ -11,22 +11,6 @@ from vegetable.models import PYTORCH_BASE_DIR, Vegetable
 PATH = os.path.join(PYTORCH_BASE_DIR, 'models', 'test.pth')
 
 
-# def recognize(request):
-#     model = core.Model.load(
-#         PATH,
-#         list(Vegetable.objects.all().order_by('id').values_list('slug', flat=True))
-#     )
-#     image_data = json.loads(request.body).get('image_data')
-#     img_file = utils.decode_base64_file(image_data)
-#     img = Image.open(img_file)
-#     labels, boxes, scores = model.predict_top(img)
-#     predictions = zip(labels, [s.item() for s in scores])
-#     return JsonResponse({
-#         'success': True,
-#         'predictions': list(predictions),
-#     })
-
-
 class RecogniseViewSet(viewsets.ModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         model = core.Model.load(
